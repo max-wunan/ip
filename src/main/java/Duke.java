8eb053f02b20884e1b92ebc0cd4353124cf91a3a
max-wunan <60435396+max-wunan@users.noreply.github.com>
2020-08-30 19:33:06 +0800

    Level 0. Greet
    Implement an initial skeletal version of the Duke that simply greets the user and exits.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,17 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        printGreetings();
+    }
+    
+    public static void printGreetings() {
+        String greeting = "____________________________________________________________\n"
+                + "Hello! I'm Duke\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n";
+
+        String bye = "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(greeting + bye);
     }
 }
