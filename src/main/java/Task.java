af55ed51dcfefdd56071b05e1e794f4f48dd3f28
max-wunan <60435396+max-wunan@users.noreply.github.com>
2020-09-23 02:44:53 +0800

    Update Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,6 +1,11 @@
 public class Task {
     protected String description;
     protected boolean isDone;
+    //protected String symbol = "[N]";
+
+    public Task() {
+        new Task("No description given");
+    }
 
     public Task(String description) {
         this.description = description;
@@ -8,7 +13,15 @@ public class Task {
     }
 
     public String getStatusIcon() {
-        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+        return (isDone ? "[\u2713]" : "[\u2718]"); //return [tick] or [X] symbols
+    }
+
+    public String getSymbol() {
+        return null;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
 }
