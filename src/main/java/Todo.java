b4ef4cc9ec15ac906c7b3649122d4282f97e039b
max-wunan <60435396+max-wunan@users.noreply.github.com>
2020-10-02 21:22:14 +0800

    Update Todo.java

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,3 +1,8 @@
+/*
+* Represents a type of task with description only
+* without any date/time
+* status is undone by default unless user marks it as done
+* */
 public class Todo extends Task {
     protected String symbol = "[T]";
 
@@ -5,6 +10,11 @@ public class Todo extends Task {
         super(content);
     }
 
+    /*
+    * Function to make it easier to print information
+    *
+    * @return String symbol "[T]"
+    * */
     @Override
     public String getSymbol() {
         return symbol;
