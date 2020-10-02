8b2eed899420ad4ca208d2456fe098c2edbd18d1
max-wunan <60435396+max-wunan@users.noreply.github.com>
2020-10-02 18:59:03 +0800

    more OOP and Level-9

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,199 +1,95 @@
-import java.util.Scanner;
-import java.util.ArrayList;
+import java.io.FileNotFoundException;
+//import java.util.Scanner;
+//import java.util.ArrayList;
 //import java.io.File;
-import java.io.FileWriter;
+//import java.io.FileWriter;
 import java.io.IOException;
 
 
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        printGreeting();
-        storeTasks();
+
+    private Storage storage;
+    private TaskList tasklist;
+    private Ui ui;
+
+
+    public Duke(String filePath) {
+        ui = new Ui();
+        storage = new Storage(filePath);
+        try {
+            tasklist = new TaskList(storage.load());
+        } catch (FileNotFoundException e) {
+            ui.printLine();
+            System.out.println("☹ OOPS!!! There is something wrong with the file: ");
+            System.out.println(e.getMessage());
+            ui.printLine();
+            tasklist = new TaskList();
+        }
     }
 
-    public static void printGreeting() {
-        String greeting = "____________________________________________________________\n"
-                + "Hello! I'm Duke\n"
-                + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "\n";
-
-        System.out.println(greeting);
-    }
-
-    public static void storeTasks() {
-        String line;
-        //Task[] tasks = new Task[100];
-        ArrayList<Task> tasks = new ArrayList<>();
-        Scanner in = new Scanner(System.in);
-        line  = in.nextLine();
-        int taskIndex = 0;
-        while (true) {
-            try {
-                if (line.equals("list")) {
-                    printList(tasks);
-                } else if (line.startsWith("done")) {
-                    int taskNo = Integer.parseInt(line.substring(5));
-                    tasks.get(taskNo-1).isDone = true;
-                    System.out.println("____________________________________________________________\n");
-                    System.out.println("Nice! I've marked this task as done:\n");
-                    System.out.println("[" + tasks.get(taskNo-1).getStatusIcon() + "]" + tasks.get(taskNo-1).description + "\n");
-                    System.out.println("____________________________________________________________\n");
-                } else if (line.startsWith("todo")) {
-                    /*tasks[taskIndex] = new Task(line);
-                taskIndex++;
-                String fromDuke = "____________________________________________________________\n"
-                        + "added: " + line + "\n"
-                        + "____________________________________________________________\n";
-                System.out.println(fromDuke);*/
-                    if (line.length() <= 5) {
-                        throw new DukeException("todo");
-                    }
-                    String content = line.substring(5);
-                    Todo newTodo = new Todo(content);
-                    tasks.add(newTodo);
-                    //[taskIndex] = newTodo;
-                    taskIndex++;
-                    printTodo(newTodo, taskIndex);
-                    /*System.out.println("____________________________________________________________\n");
-                    System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
-                    System.out.println("____________________________________________________________\n");*/
-                } else if (line.startsWith("deadline")) {
-                    if (line.length() <= 9) {
-                        throw new DukeException("deadline");
-                    }
-                    String content = line.substring(9, line.indexOf("/"));
-                    String deadline = line.substring(line.indexOf("/") + 4);
-                    Deadline newDeadline = new Deadline(content, deadline);
-                    tasks.add(newDeadline);
-                    //tasks[taskIndex] = newDeadline;
-                    taskIndex++;
-                    printDeadline(newDeadline, taskIndex);
-                } else if (line.startsWith("event")) {
-                    if (line.length() <= 6) {
-                        throw new DukeException("event");
-                    }
-                    String content = line.substring(6, line.indexOf("/"));
-                    String startTime = line.substring(line.indexOf("/") + 4);
-                    Event newEvent = new Event(content, startTime);
-                    tasks.add(newEvent);
-                    //tasks[taskIndex] = newEvent;
-                    taskIndex++;
-                    printEvent(newEvent, taskIndex);
-                } else if (line.equals("bye")) {
-                    printBye();
-                    writeToFile("src/main/java/duke.txt", tasks);
-                    break;
-                } else if (line.startsWith("delete")) {
-                    int taskNo = Integer.parseInt(line.substring(7));
-                    deleteTask(tasks, taskNo);
-                } else {
-                    throw new DukeException();
-                    /*System.out.println("____________________________________________________________\n");
-                    System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-                    System.out.println("____________________________________________________________\n");*/
+    public void run() {
+        ui.printGreeting();
+        boolean isExit = false;
+        Parser parser = new Parser();
+        while(!isExit) {
+            try{
+                String fullCommand = ui.readCommand();
+                Command newCommand = parser.analyzeCommand(fullCommand);
+                newCommand.executeCommand(tasklist, fullCommand);
+                if (newCommand.commandType == 0) {
+                    storage.writeToFile(tasklist.tasks);
+                    ui.printBye();
+                    isExit = true;
                 }
-
             } catch (DukeException e) {
-                System.out.println("____________________________________________________________\n");
-                if (e.type.equals("todo")) {
-                    System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
-                } else if (e.type.equals("deadline")) {
-                    System.out.println("☹ OOPS!!! The description of a deadline cannot be empty.");
-                } else if (e.type.equals("event")) {
-                    System.out.println("☹ OOPS!!! The description of an event cannot be empty.");
-                } else if (e.type.equals("invalid description")) {
-                    System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                switch (e.type) {
+                    case "todo": // no description given after todo
+                        ui.printLine();
+                        System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+                        ui.printLine();
+                        break;
+                    case "deadline": // no description given after deadline
+                        ui.printLine();
+                        System.out.println("☹ OOPS!!! The description of a deadline cannot be empty.");
+                        ui.printLine();
+                        break;
+                    case "event": // no description given after event
+                        ui.printLine();
+                        System.out.println("☹ OOPS!!! The description of an event cannot be empty.");
+                        ui.printLine();
+                        break;
+                    case "delete": // no index given after delete
+                        ui.printLine();
+                        System.out.println("☹ OOPS!!! I don't understand what you want to delete :-(");
+                        ui.printLine();
+                        break;
+                    case "find": // no index given after find
+                        ui.printLine();
+                        System.out.println("☹ OOPS!!! I don't understand what you want to find :-(");
+                        ui.printLine();
+                        break;
+                    case "done": // no index given after done
+                        ui.printLine();
+                        System.out.println("☹ OOPS!!! I don't understand what you have done :-(");
+                        ui.printLine();
+                    case "invalid description": // invalid command input
+                        ui.printLine();
+                        System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                        ui.printLine();
+                        break;
                 }
-                System.out.println("____________________________________________________________\n");
             } catch (IOException e) {
-                System.out.println("____________________________________________________________\n");
+                ui.printLine();
                 System.out.println("☹ OOPS!!! There is something wrong with the file: ");
                 System.out.println(e.getMessage());
-                System.out.println("____________________________________________________________\n");
+                ui.printLine();
             }
-            System.out.println("\n");
-            line = in.nextLine();
+
 
         }
-
     }
 
-    public static void printBye() {
-        String bye = "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
-        System.out.println(bye);
-    }
-
-    public static void printList(ArrayList<Task> tasks) {
-        System.out.println("____________________________________________________________\n");
-        System.out.println("Here are the tasks in your list:\n");
-        for (int i = 1; i <= tasks.size(); i++) {
-            System.out.println(i + "." + tasks.get(i-1).getSymbol() + tasks.get(i-1).getStatusIcon() + tasks.get(i-1).getDescription() + "\n");
-        }
-        System.out.println("____________________________________________________________\n");
-    }
-
-    public static void printTodo(Todo newTodo, int taskIndex) {
-        /*String content = line.substring(5);
-        Todo newTodo = new Todo(content);
-        tasks[taskIndex] = newTodo;
-        taskIndex++;*/
-        System.out.println("____________________________________________________________\n");
-        System.out.println("Got it. I've added this task: ");
-        System.out.println(newTodo.symbol + newTodo.getStatusIcon() + " " + newTodo.description);
-        System.out.println("Now you have " + taskIndex + " tasks in the list.");
-        System.out.println("____________________________________________________________\n");
-    }
-
-    public static void printDeadline(Deadline newDeadline, int taskIndex) {
-        /*String content = line.substring(9, line.indexOf("/"));
-        String deadline = line.substring(line.indexOf("/") + 4);
-        Deadline newDeadline = new Deadline(content, deadline);
-        tasks[taskIndex] = newDeadline;
-        taskIndex++;*/
-        System.out.println("____________________________________________________________\n");
-        System.out.println("Got it. I've added this task: ");
-        System.out.println(newDeadline.symbol + newDeadline.getStatusIcon() + " " + newDeadline.description + "(by: " + newDeadline.deadline + ")");
-        System.out.println("Now you have " + taskIndex + " tasks in the list.");
-        System.out.println("____________________________________________________________\n");
-    }
-
-    public static void printEvent(Event newEvent, int taskIndex) {
-        /*String content = line.substring(6, line.indexOf("/"));
-        String startTime = line.substring(line.indexOf("/") + 4);
-        Event newEvent = new Event(content, startTime);
-        tasks[taskIndex] = newEvent;
-        taskIndex++;*/
-        System.out.println("____________________________________________________________\n");
-        System.out.println("Got it. I've added this task: ");
-        System.out.println(newEvent.symbol + newEvent.getStatusIcon() + " " + newEvent.description + "(at: " + newEvent.startTime + ")");
-        System.out.println("Now you have " + taskIndex + " tasks in the list.");
-        System.out.println("____________________________________________________________\n");
-    }
-
-    public static void deleteTask(ArrayList<Task> tasks, int taskindex) {
-        String description = tasks.get(taskindex-1).getSymbol() + tasks.get(taskindex-1).getStatusIcon() + tasks.get(taskindex-1).getDescription();
-        tasks.remove(taskindex-1);
-        System.out.println("____________________________________________________________\n");
-        System.out.println("Noted. I've removed this task: ");
-        System.out.println(description);
-        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-        System.out.println("____________________________________________________________\n");
-    }
-
-    public static void writeToFile(String filePath, ArrayList<Task> tasks) throws IOException {
-        FileWriter dukeText = new FileWriter(filePath);
-        for (Task task : tasks) {
-            dukeText.write(task.getSymbol() + " | " + task.getStatusIcon() + " | " + task.getDescription());
-        }
-        dukeText.close();
+    public static void main(String[] args) {
+        new Duke("src/main/java/duke.txt").run();
     }
 }
